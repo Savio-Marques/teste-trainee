fd4228d9add1cd08760953a49d74811a88b12da6
Savio-Marques <saviomarquessouza@gmail.com>
2025-06-09 22:32:58 -0300

    feat: implementado exportação para pdf

--- src/app/todo/todo.component.ts
+++ src/app/todo/todo.component.ts
@@ -2,6 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { Todo } from '../shared/models/todo.model';
 import { TodoService } from '../shared/services/todo.service';
 
+import { jsPDF } from 'jspdf';
+
+
 @Component({
   selector: 'app-todo',
   templateUrl: './todo.component.html',
@@ -72,5 +75,40 @@ export class TodoComponent implements OnInit {
 
   get labelClearAll(){
     return 'Limpar Todos'
+  }
+
+    exportToPdf(): void {
+    const doc = new jsPDF('p', 'mm', 'a4');
+    
+    const margin = 15;
+    const lineHeight = 8;
+    let currentY = margin;
+    const pageWidth = doc.internal.pageSize.getWidth();
+    const pageHeight = doc.internal.pageSize.getHeight();
+    const today = new Date().toLocaleDateString('pt-BR');
+
+    doc.setFontSize(20);
+    doc.text('Lista de Tarefas', pageWidth / 2, currentY, { align: 'center' });
+    currentY += lineHeight * 2;
+
+    this.displayedTodos.forEach(todo => {
+      if (currentY > pageHeight - margin) {
+        doc.addPage();
+        currentY = margin;
+      }
+
+      const status = todo.completed ? '[x]' : '[ ]';
+      const taskText = `${status} ${todo.title}`;
+
+      doc.setFontSize(12);
+      doc.text(taskText, margin, currentY);
+      
+      currentY += lineHeight;
+    });
+    
+    doc.setFontSize(8);
+    doc.text(`Exportado em: ${today}`, margin, pageHeight - 10);
+
+    doc.save('lista-de-tarefas.pdf');
   }
 }
